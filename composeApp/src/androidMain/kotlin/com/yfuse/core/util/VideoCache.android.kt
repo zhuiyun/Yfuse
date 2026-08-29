f5c603d2a2fc014a967dbc9ef0f0530dc88bc2e8
zhuiyun <zhuiyun0413@gmail.com>
2026-08-29 18:08:45 +0800

    fix(android): opt in cache utility to Media3 API

--- composeApp/src/androidMain/kotlin/com/yfuse/core/util/VideoCache.android.kt
+++ composeApp/src/androidMain/kotlin/com/yfuse/core/util/VideoCache.android.kt
@@ -1,15 +1,18 @@
 package com.yfuse.core.util
 
+import androidx.media3.common.util.UnstableApi
 import com.yfuse.feature.player.VideoCachePool
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
+@OptIn(UnstableApi::class)
 actual suspend fun clearVideoCache(): Long =
     withContext(Dispatchers.IO) {
         val context = imageCacheContext ?: return@withContext 0L
         VideoCachePool.clear(context)
     }
 
+@OptIn(UnstableApi::class)
 actual suspend fun videoCacheUsageBytes(): Long =
     withContext(Dispatchers.IO) {
         val context = imageCacheContext ?: return@withContext 0L
